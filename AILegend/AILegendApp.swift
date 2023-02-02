a5812b784762fdaa96f8478e8bb73c86d0c507f7
kaidenpoon <kaidenpoon@gmail.com>
2023-02-02 09:37:03 +0800

    Initial Commit

---
+++ AILegend/AILegendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AILegendApp.swift
+//  AILegend
+//
+//  Created by Kaiden Poon on 2/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct AILegendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
